9ca587f8170fe71497b9440a13e3da9c1be04f52
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-08 13:46:51 -0500

    created footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+// import { useSelector } from "react-redux"
+
+function Footer(){
+  // const pizzaStore = useSelector(store => store.pizzaStore)
+  return (
+  <footer className="App-footer">
+    This is a footer
+  </footer>
+);
+}
+
+export default Footer
