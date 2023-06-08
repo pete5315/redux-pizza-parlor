18cc9a9494dfe23608598a4f789be3b980304fa7
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-08 14:02:35 -0500

    added storeinstance and passed store to provider

--- src/index.js
+++ src/index.js
@@ -6,38 +6,41 @@ import { Provider } from 'react-redux';
 
 
 const pizzaList = (state = [], action) => {
-  // TODO: Save all pizza options from the server
   if (action.type === `GET_PIZZAS`) {
-    // this will replace the product list, payload is array of all products
     return  action.payload 
   }
   return state;
 };
 
 const orderList = (state = [], action) => {
-  // TODO: Save all pizza options from the server
   if (action.type === `GET_ORDERS`) {
-    // this will replace the product list, payload is array of all products
     return  action.payload 
   }
   return state;
 };
 
 const formData = (state = [], action) => {
-  // TODO: Save all pizza options from the server
   if (action.type === `GET_FORM`) {
-    // this will replace the product list, payload is array of all products
     return  action.payload 
   }
   return state;
 };
+
+const storeInstance = createStore(
+  combineReducers({
+    pizzaList,
+    orderList,
+    formData
+  }),
+  applyMiddleware(logger),
+);
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider>
+        <Provider store={storeInstance}>
         <App />
         </Provider>
     </React.StrictMode>
