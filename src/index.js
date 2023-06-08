5599432d86ad3d76b11813e9ce280a0e6352dc67
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-08 10:41:50 -0500

    added reducers to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+const pizzaList = (state = [], action) => {
+  // TODO: Save all pizza options from the server
+  if (action.type === `GET_ORDERED_PIZZAS`) {
+    // this will replace the product list, payload is array of all products
+    return  action.payload 
+  }
+  return state;
+};
+
+const orderList = (state = [], action) => {
+  // TODO: Save all pizza options from the server
+  if (action.type === `GET_ORDERS`) {
+    // this will replace the product list, payload is array of all products
+    return  action.payload 
+  }
+  return state;
+};
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
