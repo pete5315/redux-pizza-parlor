ee329d304d0760e39894bbec148bf0a9911e258a
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-08 13:50:01 -0500

    added checkout component, removed unneeded comments from footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
-// import { useSelector } from "react-redux"
+
 
 function Footer(){
-  // const pizzaStore = useSelector(store => store.pizzaStore)
   return (
   <footer className="App-footer">
     This is a footer
