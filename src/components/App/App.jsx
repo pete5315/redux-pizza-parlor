96caca21a128fdcd75baec047f18653b99119977
Zackaria Antar <antarzack@gmail.com>
2023-06-08 10:47:31 -0500

    created axios get request named getPizzaData with dispatch action, imported useEffect, and put getPizzaData in the useEffect on App.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import Header from '../Header/Header';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    getPizzaData()
+  })
+
+  const getPizzaData = () =>{
+    axios.get('/')
+    .then((response)=>{
+      console.log(response.data);
+      dispatch({type:'GET_PIZZAS', payload: response.data })
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
 
   return (
     <div className='App'>
