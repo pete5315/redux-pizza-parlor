ae244917ef964e3580076f560c4be78b8dbfb8fa
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-09 11:29:34 -0500

    added next button to pizza list component

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,14 +1,21 @@
 import { useSelector } from "react-redux"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import PizzaCard from "../PizzaCard/PizzaCard"
 
 function PizzaList(){
     const pizzaList = useSelector(store=> store.pizzaList)
+    const history = useHistory();
+
+    function handleSubmit() {
+      history.push('/form')
+    }
     return (
 		<div>
 			{/*TODO change to be carousel as container for cards */}
 			{pizzaList.map((pizza) => (
 				<PizzaCard key={pizza.id} pizza={pizza} />
 			))}
+      <button onClick={handleSubmit}>Next</button>
 		</div>
 	);
 }
