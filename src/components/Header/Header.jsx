77579f64a2793f26fd2d94d6255d3909cd608ea5
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-09 14:22:34 -0500

    na

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,36 @@
 import { useSelector } from "react-redux"
 
 function Header(){
-let totalCost=0;
+  let totalCost=0;
   const orderList = useSelector(store => store.orderList)
+  const [address, setAddress] = useState("");
   for (let item of orderList){
     totalCost += Number(item.price)
 
   }
+  if (address===) {
+    alert("YOU MUST BUY THE PIZZA");
+    setAdded(!added);
+  }
     return (
 		<header className="App-header">
 			<h1 className="App-title">Prime Pizza</h1>
+      {added ? (
 			<span className="cart-total">
-        <p>
-				<span>
-					<button>🛒</button>
-				</span>
-				<span>
-					Total: ${totalCost.toFixed(2)}
+        
+      <p>
+      <span>
+        <button>🛒</button>
+      </span>
+      <span>
+        Total: ${totalCost.toFixed(2)}
+      </span>
+      </p>
+    </span>
+        ) : (
+          <></>
+        )}
 
-				</span>
-        </p>
-			</span>
 		</header>
 	);
 }
